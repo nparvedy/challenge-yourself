ede99efcc77e32d9b534df8350eb8b8506f38e92
parvedyn <nparvedysimplon@gmail.com>
2019-08-28 14:07:14 +0200

    Correction v2 + ajout add2.php

---
+++ add-test.php
@@ -0,0 +1,36 @@
+<?php 
+    /* $add = function($premierNombre, $deuxiemeNombre){
+           if ($premierNombre >= - 100 && $premierNombre <= 1000 && $deuxiemeNombre >= - 100 && $deuxiemeNombre <= 1000){
+                $resultat = $premierNombre + $deuxiemeNombre;
+                echo $resultat;
+            }else {
+                echo "err\n";
+            }
+        };*/
+        $tableauNombre = [5,3,-150,2,5,7000];
+        echo "Le tableau sans vérification : <br />";
+        print_r($tableauNombre);
+        
+
+        $addTableau = function($tableauNombre){
+            for ($i = 0; $i < count($tableauNombre); $i++){
+                if ($tableauNombre[$i] >= - 100 && $tableauNombre[$i] <= 1000){
+                    $newTableauNombre[$i] = $tableauNombre[$i];
+                    $resultat = array_sum($newTableauNombre);
+                    ?><p id="erreur">
+                    <?php
+                    echo "<style>#erreur{color:green;}</style>Le chiffre $tableauNombre[$i] répond à la condition";
+                    ?>
+                    </p>
+                    <?php
+                }else {
+                    echo "<style>#right{color:red;}</style><p id='right'>Le nombre $tableauNombre[$i] ne correspond pas à la condition</p>";
+                }
+            }
+            echo "La somme des nombres vérifier par la condition '-100 ≤ param1 ≤ 1000' vaut : $resultat";
+         };
+ 
+
+       // $add(1,2);
+        $addTableau($tableauNombre);
+    ?>
--- add.php
+++ add.php
@@ -1,47 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php 
-    /* $add = function($premierNombre, $deuxiemeNombre){
-           if ($premierNombre >= - 100 && $premierNombre <= 1000 && $deuxiemeNombre >= - 100 && $deuxiemeNombre <= 1000){
-                $resultat = $premierNombre + $deuxiemeNombre;
-                echo $resultat;
-            }else {
-                echo "err\n";
-            }
-        };*/
-        $tableauNombre = [5,3,-150,2,5,7000];
-        echo "Le tableau sans vérification : <br />";
-        print_r($tableauNombre);
-        
+<?php 
+    $add = function(){
+        $resultat = 0;
+        foreach (func_get_args($resultat) as $value){
 
-        $addTableau = function($tableauNombre){
-            for ($i = 0; $i < count($tableauNombre); $i++){
-                if ($tableauNombre[$i] >= - 100 && $tableauNombre[$i] <= 1000){
-                    $newTableauNombre[$i] = $tableauNombre[$i];
-                    $resultat = array_sum($newTableauNombre);
-                    ?><p id="erreur">
-                    <?php
-                    echo "<style>#erreur{color:green;}</style>Le chiffre $tableauNombre[$i] répond à la condition";
-                    ?>
-                    </p>
-                    <?php
-                }else {
-                    echo "<style>#right{color:red;}</style><p id='right'>Le nombre $tableauNombre[$i] ne correspond pas à la condition</p>";
-                }
+            if ($value >= - 100 && $value <= 1000){
+                $resultat += $value;
             }
-            echo "La somme des nombres vérifier par la condition '-100 ≤ param1 ≤ 1000' vaut : $resultat";
-         };
- 
+        }
+        echo $resultat;
+    };
 
-       // $add(1,2);
-        $addTableau($tableauNombre);
-    ?>
-</body>
-</html>
+    $add(5,5,-300,2000,4,7);
+?>
