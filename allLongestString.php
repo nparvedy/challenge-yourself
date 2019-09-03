feda02cc52b0754540fabb33aa51be200b8f0666
parvedyn <nparvedysimplon@gmail.com>
2019-09-03 17:13:42 +0200

    NE REGARDE PAS MON CODE !

--- allLongestString.php
+++ allLongestString.php
@@ -1,15 +1,21 @@
 <?php 
 $allLongestString = function($array){
+    $taille = 0;
     $tableau = [];
     for ($i = 0; $i < count($array); $i++){
-        if (count($array) > 10){
-            if (strlen($array[$i]) >= 1 && strlen($array[$i]) <= 10){
-                if (strlen($array[$i]) >= 3 && strlen($array[$i]) <= 10){
-                    array_push($tableau, "$array[$i]");
-                }
-            }else{
-                return "err\n";
+        if (count($array) >= 1 && count($array) <= 10 && strlen($array[$i]) >= 1 && strlen($array[$i]) <= 10 ){
+            if ($taille <= strlen($array[$i])){
+                $taille = strlen($array[$i]);
+                array_push($tableau, "$array[$i]");
             }
+        
+        }else {
+            return "err\n";
+        }
+    }
+    for ($i = 0; $i < count($tableau); $i++){
+        if ($taille > strlen($tableau[$i])){
+            unset($tableau[$i]);
         }
     }
     return $tableau;
