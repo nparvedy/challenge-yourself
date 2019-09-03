3b78b9f3a6ce7e40b28d9536fe02e0dc139d77ba
parvedyn <nparvedysimplon@gmail.com>
2019-09-03 16:22:59 +0200

    final !

--- allLongestString.php
+++ allLongestString.php
@@ -1,20 +1,18 @@
 <?php 
-$tableau = ["aba", "aa", "ad", "vcd", "aba"];
 $allLongestString = function($array){
+    $tableau = [];
     for ($i = 0; $i < count($array); $i++){
         if (strlen($array[$i]) >= 1 && strlen($array[$i]) <= 10){
-            if (strlen($array[$i]) >= 1 && strlen($array[$i]) <= 2){
-                unset($array[$i]);
+            if (strlen($array[$i]) >= 3 && strlen($array[$i]) <= 10){
+                array_push($tableau, "$array[$i]");
             }
-            
         }else{
             return "err\n";
         }
     }
-    return $array;
+    return $tableau;
 };
 
-$allLongestString($tableau);
 ?>
 
 
